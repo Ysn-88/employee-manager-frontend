0df789dc668d231ca71569fdce45055b5f82bb70
Yassine Akdim <yassine.akdim@gmail.com>
2023-08-17 09:00:10 +0200

    get names from Backend/environments/Api

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apyServerUrl = 'http://localhost:8080/api/employee';
+  private apyServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
@@ -17,5 +18,13 @@ export class EmployeeService {
 
   public addEmployee(employee: Employee): Observable<Employee> {
     return this.http.post<Employee>(`${this.apyServerUrl}/employee/add`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apyServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apyServerUrl}/employee/delete/${employeeId}`);
   }
 }
