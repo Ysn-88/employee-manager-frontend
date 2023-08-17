1d36217ef2b4a750c890681f72dfc5eba9d16c73
Yassine Akdim <yassine.akdim@gmail.com>
2023-08-17 07:39:50 +0200

    update

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private apyServerUrl = 'http://localhost:8080/api/employee';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apyServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apyServerUrl}/employee/add`, employee);
+  }
 }
